b653d4b5ca38ed5d1bb5a1669a5b3219637f5397
aravindhvetri <2002aravinthv@gmail.com>
2025-05-30 13:03:15 +0530

    Aari's First initial changes 30/05/2025 01:03pm

---
+++ src/webparts/requestApproval/components/MainComponent.tsx
@@ -0,0 +1,13 @@
+//Default Imports:
+import * as React from "react";
+//Styles Imports:
+import "../../../External/style.css";
+const MainComponent = ({ context }) => {
+  return (
+    <div>
+      <h1>Request Approval</h1>
+      <p>This is the main component for the Request Approval web part.</p>
+    </div>
+  );
+};
+export default MainComponent;
