e2a959dd3b5b530647eeaa7aca8e0015e8726807
aravindhvetri <2002aravinthv@gmail.com>
2025-05-30 12:02:18 +0530

    First push to main 30/05/2025

---
+++ src/webparts/requestApproval/components/IRequestApprovalProps.ts
@@ -0,0 +1,7 @@
+export interface IRequestApprovalProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/requestApproval/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IRequestApprovalWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'RequestApprovalWebPartStrings' {
+  const strings: IRequestApprovalWebPartStrings;
+  export = strings;
+}
